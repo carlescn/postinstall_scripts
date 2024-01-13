cd4728a99d5daff52e60ead5a6c1b4ec1306484e
carlescn <100298997+carlescn@users.noreply.github.com>
2024-01-13 23:47:12 +0100

    feat: add run_scripts.sh to run all scripts.

---
+++ run_scripts.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+###################################################################
+# Script Name : run_scripts.sh
+# Description : Runs all the scripts using run-parts.
+# Args        : None
+# Author      : CarlesCN
+# E-mail      : carlesbioinformatics@gmail.com
+# License     : GNU General Public License v3.0
+###################################################################
+
+set -e -u -o pipefail
+
+if [[ $EUID -ne 0 ]]; then echo "Please run as root"; exit 1; fi
+
+
+path="${PWD}/scripts/"
+
+echo ''
+echo 'WARNING: this will run the following scripts with root privileges:'
+run-parts --test "${path}"
+echo ''
+
+read -p 'Are you sure? [y/N] ' -n 1 -r
+echo ''
+
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  run-parts "${path}"
+else
+  echo "Cancelled"
+fi
