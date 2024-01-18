27aa6b7e5e298e38adf1a3ffada3fb8106f0bd7c
carlescn <100298997+carlescn@users.noreply.github.com>
2024-01-18 21:11:26 +0100

    fix: append to dnf config instead of overwriting it

--- scripts/10_setup_dnf_speedup.sh
+++ scripts/10_setup_dnf_speedup.sh
@@ -12,5 +12,5 @@
 set -e -u -o pipefail
 
 
-echo "max_parallel_downloads=10" > /etc/dnf/dnf.conf
-echo "fastestmirror=True" > /etc/dnf/dnf.conf
+echo "max_parallel_downloads=10" >> /etc/dnf/dnf.conf
+echo "fastestmirror=True" >> /etc/dnf/dnf.conf
