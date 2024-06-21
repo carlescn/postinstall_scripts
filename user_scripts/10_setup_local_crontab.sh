b10bcfb80777d2846be480f12905dc1211b74494
carlescn <100298997+carlescn@users.noreply.github.com>
2024-06-22 00:01:48 +0200

    feat: improve script 10_setup_local_crontab

--- user_scripts/10_setup_local_crontab.sh
+++ user_scripts/10_setup_local_crontab.sh
@@ -9,6 +9,19 @@
 # License     : GNU General Public License v3.0
 ###################################################################
 
-set -e -u -o pipefail
+set -u -o pipefail # no -e because must let crontab fail if it's empty
 
-crontab ../resources/crontab_anacron
+task='@hourly /usr/sbin/anacron -s -t $HOME/.anacron/anacrontab -S $HOME/.anacron/spool'
+temp_file='./.crontab.tmp'
+
+# backup crontab (exits with code 1 if empty)
+crontab -l 2>/dev/null > "$temp_file"
+# append new line
+echo "$task" >> "$temp_file"
+# install new crontab
+crontab "$temp_file"
+
+rm "$temp_file"
+
+echo "crontab updated:"
+crontab -l
